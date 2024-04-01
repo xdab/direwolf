24c27f58f7d9857fb7a0b1f7e99509ccfa427633
PW <przemyslawxdab@gmail.com>
2024-04-01 21:43:44 +0200

    Remove unused files

--- scripts/telemetry-toolkit/telem-seq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Generate sequence number as described here:
-# https://github.com/wb2osz/direwolf/issues/9
-#
-SEQ=`cat /tmp/seq 2>/dev/null`
-SEQ=$(expr \( $SEQ + 1 \) % 1000)
-echo $SEQ | tee /tmp/seq
